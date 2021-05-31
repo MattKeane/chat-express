2bd73f9097eef3d98339a142e209140a9e43ee1c
Matt Keane <mattpkeane@gmail.com>
2021-05-31 09:24:30 -0500

    user registration route works

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const prisma = require('../prisma/client');
 
 router.get('/', (req, res) => {
 	res.send('Auth controller works!');
 });
 
-router.post('/register', (req, res, next) => {
-
+router.post('/register', async (req, res) => {
+	try {
+		const emailExists = await prisma.user.findUnique({
+			where: {
+				email: req.body.email,
+			}
+		});
+		const usernameExists = await prisma.user.findUnique({
+			where: {
+				username: req.body.username,
+			}
+		});
+		if (emailExists) {
+			const body = {
+				message: 'User with that email already exists!',
+				data: {},
+			};
+			res.status(400).json(body);
+		} else if (usernameExists) {
+			const body = {
+				message: 'User with that username already exists!',
+				data: {},
+			};
+			res.status(400).json(body)
+		} else {
+			const salt = bcrypt.genSaltSync(10);
+			req.body.password = bcrypt.hashSync(req.body.password, salt);
+			const createdUser = await prisma.user.create({
+				data: req.body,
+			});
+			delete createdUser.password;
+			const body = {
+				message: "User successfully created!",
+				data: createdUser,
+			};
+			res.status(201).json(body);
+		}
+	} catch (err) {
+		const d = new Date();
+		console.log(`${d.toLocaleString()}: ${err}`)
+		const body = {
+			message: err,
+			data: {},
+		};
+		res.status(400).json(body);
+	}
 });
 
 module.exports = router;
